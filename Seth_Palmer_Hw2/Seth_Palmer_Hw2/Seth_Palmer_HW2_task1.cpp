2d21968585ffaf5f1d0b0c0f8c44ac537886af22
sethpalmer94 <sethpalmer@mail.weber.edu>
2017-09-25 01:16:41 -0600

    missing file

---
+++ Seth_Palmer_Hw2/Seth_Palmer_Hw2/Seth_Palmer_HW2_task1.cpp
@@ -0,0 +1,109 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  housePrice.cpp
+ *       	Usage:  ./housePrice.cpp
+ *    Description:  Ask user to specify the address and price
+ *    				of two house that are on sale
+ *    				It prints out which of the two houses is cheaper
+ *       Compiler:  g++
+ *         Author:  Dr. Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  
+ * =====================================================================================
+ */
+#include <iostream>
+using namespace std;
+/*  Structures/Constants */
+struct house
+{
+	int streetNum;
+	char streetName[30];
+	double price;
+};
+
+// Function prototypes
+struct house InputHouse(void);
+void PrintHouse(int houseNum, struct house h);  
+int ComparePrices(struct house h1, struct house h2);
+
+/* Main Program */
+int main(int argc, char* argv[])
+{
+	cout<<"Enter info for first house\n";
+	struct house firstHouse = InputHouse();
+	cout<<"\nEnter info for second house\n";
+	struct house secondHouse = InputHouse();
+
+	PrintHouse(1, firstHouse);  
+	printf("\n\n");
+	PrintHouse(2, secondHouse);
+	printf("\n\n");
+
+	int comparison = ComparePrices(firstHouse, secondHouse);
+    /* Based on comparison value, use a switch 
+     * statement to display all posibilities:
+     * 1) "the first house is cheaper" 
+     * 2) "the two houses are equally expensive"
+     * 3) "the second house is cheaper" 
+     * 4) "invalid comparison value there must be a bug"
+     */
+	switch (comparison) {
+	case 1:
+		cout << "the first house is cheaper" << endl;
+		break;
+	case 2:
+		cout << "the two houses are equally expansive" << endl;
+		break;
+	case 3:
+		cout << "the second house is cheaper" << endl;
+		break;
+	 default:
+		 cout << "invalid comparison value there must be a bug" << endl;
+		 break;
+	}
+
+
+	system("pause"); //keep window open in visual studio
+	return 0;
+}
+
+/* Function Defenitions */
+struct house InputHouse(void)
+{
+	house placeHolder;
+	cout << "please enter the street number" << endl;
+	cin >> placeHolder.streetNum;
+	cout << "\nplease enter the street name" << endl;
+	cin >> placeHolder.streetName;
+	cout << "\n please enter the price" << endl;
+	cin >> placeHolder.price;
+
+	return placeHolder;
+}
+
+	void PrintHouse(int houseNum, struct house h)
+{
+	cout << houseNum << " house: at " << h.streetNum << " " << h.streetName << " for $" << h.price;
+}
+
+int ComparePrices(struct house h1, struct house h2)
+{
+	if (h1.price < h2.price)
+	{
+		return 1;
+	}
+	else if (h1.price == h2.price)
+	{
+		return 2;
+	}
+	else if (h1.price > h2.price)
+	{
+		return 3;
+	}
+	else
+	{
+		return -1;
+	}
+
+
+}
